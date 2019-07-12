41116328a9932d3b9f809a5b513fab5773a53651
Pur2all <39830676+Pur2all@users.noreply.github.com>
2019-07-12 18:00:59 +0200

    Add retrieve wishlist servlet

--- src/control/servlet/retrieve/RetrieveProductsInWishlist.java
+++ src/control/servlet/retrieve/RetrieveProductsInWishlist.java
@@ -1,11 +1,18 @@
 package control.servlet.retrieve;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.LinkedList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.bean.WishlistProduct;
+import model.dao.DBConnectionPool;
+import model.dao.WishlistDAO;
 
 @WebServlet("/RetrieveProductsInWishlist")
 public class RetrieveProductsInWishlist extends HttpServlet 
@@ -14,7 +21,20 @@ public class RetrieveProductsInWishlist extends HttpServlet
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
+		WishlistDAO wishlistDAO=new WishlistDAO((DBConnectionPool) getServletContext().getAttribute("DriverManager"));
 		
+		try
+		{
+			LinkedList<WishlistProduct> wishlistProducts=(LinkedList<WishlistProduct>) wishlistDAO.doRetrieveAll(null);
+			
+			request.setAttribute("wishlistProducts", wishlistProducts);
+			
+			getServletContext().getRequestDispatcher("Wishlist").forward(request, response);
+		} 
+		catch (SQLException sqlException)
+		{
+			sqlException.printStackTrace();
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
